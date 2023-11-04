29490e7cab54778bead74779daf6b4fe0b5cf082
aciken <adrianmarton2006@gmail.com>
2023-11-04 19:08:58 +0100

    almost over with all functionalities

--- main.js
+++ main.js
@@ -1,10 +1,25 @@
 const container = document.getElementById('container');
+const colorInput = document.getElementById('colorInput');
+const clearBtn = document.getElementById('clearBtn');
+const choseSize = document.querySelectorAll('.choseSize');
+const grid = document.querySelector('.grid');
+
+
+
+let colorValue = 'black';
+
+
+colorInput.addEventListener('blur', ()=>{
+    colorValue = colorInput.value;
+})
+
+
 
 
 const rowFunc = (num) =>{
     const wholeDivRow = document.createElement('div');
     wholeDivRow.classList.add('wholeDivRow');  
-    container.appendChild(wholeDivRow); 
+    grid.appendChild(wholeDivRow); 
     for(let i = 0; i < num; i++){
        const rowDiv = document.createElement('div');
        rowDiv.classList.add('rowDiv');
@@ -20,10 +35,55 @@ const colFunc = (row ,col) =>{
 
 container.addEventListener('mouseover', (e) =>{
     if(e.target.classList.value === 'rowDiv'){
-        e.target.style.cssText = 'background-color: black';
+        e.target.style.cssText = `background-color: ${colorValue}`;
     }
 
 })
 
-colFunc(16, 16);
 
+colFunc(10,10)
+
+clearBtn.addEventListener('click', () =>{
+    rowDiv.forEach(element =>{
+        console.log('asd');
+       element.style.cssText = 'background-color: white';
+    });
+
+});
+
+
+
+choseSize.forEach(element =>{
+    element.addEventListener('click', (e) =>{
+       if(e.target.textContent === '10x10'){
+        while(grid.firstChild){
+            grid.removeChild(grid.firstChild);
+
+        }
+        rowDiv.forEach(element =>{
+            element.style.cssText = 'min-width: 40px; min-height: 40px;';
+        })
+        colFunc(10,10);
+       } else if(e.target.textContent === '20x20'){
+        while(grid.firstChild){
+            grid.removeChild(grid.firstChild);
+        }
+        rowDiv.forEach(element =>{
+            element.style.cssText = 'min-width: 20px; min-height: 20px;';
+        })
+        colFunc(20, 20)
+       } else {
+        while(grid.firstChild){
+            grid.removeChild(grid.firstChild);
+        }
+        rowDiv.forEach(element =>{
+            element.style.cssText = 'min-width: 10px; min-height: 10px;';
+        })
+        colFunc(30, 30);
+       }
+    })
+})
+
+
+
+const rowDiv = document.querySelectorAll('.rowDiv');
